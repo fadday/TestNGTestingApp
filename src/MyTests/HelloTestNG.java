c51d60957e7d085385435695d3f6e96954d16f4d
Artem Antonov <fadday.x@gmail.com>
2014-07-28 16:04:07 +0600

    Initial commit

---
+++ src/MyTests/HelloTestNG.java
@@ -0,0 +1,22 @@
+package MyTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Artem on 28.07.2014.
+ */
+
+public class HelloTestNG {
+    @Test
+    public void Test()
+    {
+        System.out.println("Hello TestNG");
+    }
+
+    @Test
+    public void Test2()
+    {
+        Assert.assertEquals(1, 2);
+    }
+}
